03b7e5252cfc7298e6fe0a787ed6bab76613de13
jtimberman <joshua@opscode.com>
2013-01-24 20:37:26 -0700

    [COOK-2266] - pin runit dependency

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license           "Apache 2.0"
 description       "Installs memcached and provides a define to set up an instance of memcache via runit"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.1.2"
-depends           "runit"
+depends           "runit", "<= 0.16.2"
 depends           "yum"
 
 recipe "memcached", "Installs and configures memcached"
