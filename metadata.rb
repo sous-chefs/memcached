ee10ad47e34f4b5e935d9e992db2ca9ca633ebb7
Antek Baranski <abaranski@roblox.com>
2013-07-16 16:07:27 -0700

    Adding an option to specify a log file

--- metadata.rb
+++ metadata.rb
@@ -34,3 +34,8 @@ attribute "memcached/listen",
   :display_name => "Memcached IP Address",
   :description => "IP address to use for memcached instance",
   :default => "0.0.0.0"
+
+attribute "memcached/logfilename",
+          :display_name => "Memcached logfilename",
+          :description => "The filename used to log memcached",
+          :default => "memcached.log"
