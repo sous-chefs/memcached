f361cba575441d44836d123930a58227000881ae
Seth Vargo <sethvargo@gmail.com>
2013-10-04 14:12:53 -0400

    Use Berkshelf to instal dependencies on Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,6 @@
+require 'berkshelf'
 require 'chefspec'
+
+Berkshelf.ui.mute do
+  Berkshelf::Berksfile.from_file('Berksfile').install(path: 'vendor/cookbooks')
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe 'memcached::default' do
-  let(:chef_run) { ChefSpec::ChefRunner.new(cookbook_path: '..').converge(described_recipe) }
+  let(:chef_run) { ChefSpec::ChefRunner.new.converge(described_recipe) }
 
   it 'installs the libmemcache-dev package' do
     expect(chef_run).to install_package('libmemcache-dev')
   end
 
   context 'on rhel' do
-    let(:chef_run) { ChefSpec::ChefRunner.new(cookbook_path: '..', platform: 'redhat', version: '6.3').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ChefRunner.new(platform: 'redhat', version: '6.3').converge(described_recipe) }
     let(:template) { chef_run.template('/etc/sysconfig/memcached') }
 
     it 'writes the /etc/sysconfig/memcached' do
@@ -24,7 +24,7 @@ describe 'memcached::default' do
   end
 
   context 'on smartos' do
-    let(:chef_run) { ChefSpec::ChefRunner.new(cookbook_path: '..', platform: 'smartos', version: 'joyent_20130111T180733Z').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ChefRunner.new(platform: 'smartos', version: 'joyent_20130111T180733Z').converge(described_recipe) }
 
     it 'enables the memcached service' do
       expect(chef_run).to set_service_to_start_on_boot('memcached')
@@ -32,7 +32,7 @@ describe 'memcached::default' do
   end
 
   context 'on ubuntu' do
-    let(:chef_run) { ChefSpec::ChefRunner.new(cookbook_path: '..', platform: 'ubuntu', version: '12.04').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ChefRunner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe) }
     let(:template) { chef_run.template('/etc/memcached.conf') }
 
     it 'writes the /etc/memcached.conf' do
