ee296e7085720fdb02f42d6f5978e40c593526fb
Niels Kristensen <niels@kristensen.io>
2013-10-07 10:14:59 +0200

    add UDP support

--- recipes/default.rb
+++ recipes/default.rb
@@ -81,6 +81,7 @@ else
       :listen          => node['memcached']['listen'],
       :user            => node['memcached']['user'],
       :port            => node['memcached']['port'],
+      :udp_port        => node['memcached']['udp_port'],
       :maxconn         => node['memcached']['maxconn'],
       :memory          => node['memcached']['memory'],
       :max_object_size => node['memcached']['max_object_size']
