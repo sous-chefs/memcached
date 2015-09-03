1bd6f1eef012ab4fcba83b562a1fa9925a84bfa2
anthony caiafa <2600.ac@gmail.com>
2015-09-03 09:14:24 -0400

    changing license

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer 'Anthony Caiafa'
 maintainer_email '2600.ac@gmail.com'
-license 'All rights reserved'
+license 'Apache 2.0'
 description 'Installs/Configures memcached'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
