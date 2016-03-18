ff0a4a09dd3b902dfe39e125ae1e51d9835d0da3
Tim Smith <tsmith84@gmail.com>
2016-03-17 22:11:18 -0700

    Remove some now bogus specs

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -9,7 +9,7 @@ describe 'memcached::default' do
   end
 
   context 'on rhel' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'redhat', version: '6.3').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'centos', version: '6.7').converge(described_recipe) }
 
     it 'creates memcached group' do
       expect(chef_run).to create_group('memcached')
@@ -17,40 +17,14 @@ describe 'memcached::default' do
 
     let(:template) { chef_run.template('/etc/sysconfig/memcached') }
 
-    it 'writes the /etc/sysconfig/memcached' do
-      expect(template).to be
-      expect(template.owner).to eq('root')
-      expect(template.group).to eq('root')
-      expect(template.mode).to eq('0644')
-    end
-
     it 'notifies the service to restart' do
       expect(template).to notify('service[memcached]').to(:restart)
     end
   end
 
-  context 'on smartos' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'smartos', version: 'joyent_20130111T180733Z').converge(described_recipe) }
-
-    it 'enables the memcached service' do
-      expect(chef_run).to enable_service('memcached')
-    end
-
-    it 'creates nogroup group' do
-      expect(chef_run).to create_group('nogroup')
-    end
-  end
-
   context 'on ubuntu' do
-    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe) }
+    let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
     let(:template) { chef_run.template('/etc/memcached.conf') }
-
-    it 'writes the /etc/memcached.conf' do
-      expect(template).to be
-      expect(template.owner).to eq('root')
-      expect(template.group).to eq('root')
-      expect(template.mode).to eq('0644')
-    end
 
     it 'notifies the service to restart' do
       expect(template).to notify('service[memcached]').to(:restart)
