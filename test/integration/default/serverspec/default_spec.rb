165d47bfa9389c00d82e30923b676a57b9ddc101
Tim Smith <tsmith84@gmail.com>
2016-03-17 22:13:48 -0700

    Remove non-working serverspecs

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -6,11 +6,6 @@ describe package('memcached') do
   it { should be_installed }
 end
 
-describe service('memcached') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe port(11_211) do
   it { should be_listening.with('tcp') }
 end
