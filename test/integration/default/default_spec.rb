c3a00cbc3ea9b52b33a2e1771dbcd68c103256ce
Tim Smith <tsmith@chef.io>
2016-05-23 10:15:31 -0700

    Swap serverspec for inspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ test/integration/default/default_spec.rb
@@ -0,0 +1,14 @@
+
+describe package('memcached') do
+  it { should be_installed }
+end
+
+describe service('memcached') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(11_211) do
+  it { should be_listening }
+end
---
+++ test/integration/instance/instance_spec.rb
@@ -0,0 +1,24 @@
+
+describe package('memcached') do
+  it { should be_installed }
+end
+
+describe service('memcached_backend_cache') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('memcached_web_cache') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(11_212) do
+  it { should be_listening }
+end
+
+describe port(11_213) do
+  it { should be_listening }
+end
