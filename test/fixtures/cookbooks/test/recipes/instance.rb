62e0faf8e637a9cbb4c8fea4eca5f71eff5d94f0
Tim Smith <tsmith@chef.io>
2016-05-23 15:16:37 -0700

    Fix / expand instances test recipe

--- test/fixtures/cookbooks/test/recipes/instance.rb
+++ test/fixtures/cookbooks/test/recipes/instance.rb
@@ -1,9 +1,16 @@
 memcached_instance 'web_cache' do
   port 11_212
   memory 128
+  action [:start, :enable]
 end
+
+user 'memcached_other_user'
 
 memcached_instance 'backend_cache' do
   port 11_213
   memory 128
-end
+  ulimit 31337
+  threads 10
+  user 'memcached_other_user'
+  action [:start, :enable]
+ends
