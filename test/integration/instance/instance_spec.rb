5ccbaf1d2aa55497a9ed37bf16f8cdc7f8349501
Tim Smith <tsmith@chef.io>
2016-06-28 09:04:03 -0700

    Fix inspec test
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/instance/instance_spec.rb
+++ test/integration/instance/instance_spec.rb
@@ -15,7 +15,7 @@ describe service('memcached_web_cache') do
   it { should be_running }
 end
 
-describe service('painful_cache') do
+describe service('memcached_painful_cache') do
   it { should be_installed }
   it { should be_enabled }
   it { should be_running }
