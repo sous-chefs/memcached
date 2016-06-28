431579e4d223426e28ce42ceb8266aa81cfe4340
Tim Smith <tsmith@chef.io>
2016-06-28 15:05:02 -0700

    Set the ports in the test recipe

--- test/fixtures/cookbooks/test/recipes/instance.rb
+++ test/fixtures/cookbooks/test/recipes/instance.rb
@@ -1,5 +1,6 @@
 memcached_instance 'web_cache' do
   port 11_212
+  udp_port 11_212
   memory 64
   action [:start, :enable]
 end
@@ -8,6 +9,7 @@ user 'memcached_other_user'
 
 memcached_instance 'backend_cache' do
   port 11_213
+  udp_port 11_213
   memory 64
   ulimit 31_337
   threads 10
@@ -17,6 +19,7 @@ end
 
 memcached_instance_sysv_init 'painful_cache' do
   port 11_214
+  udp_port 11_214
   memory 64
   ulimit 31_337
   threads 10
