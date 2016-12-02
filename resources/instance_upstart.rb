d313ef2597d7582af3890bc9e8cdef6976df94b1
Tim Smith <tsmith@chef.io>
2016-12-02 15:48:19 -0800

    Update copyright headers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/instance_upstart.rb
+++ resources/instance_upstart.rb
@@ -1,9 +1,9 @@
 #
-# Cookbook Name:: memcached
+# Cookbook:: memcached
 # resource:: instance_upstart
 #
-# Author Tim Smith <tsmith@chef.io>
-# Copyright 2016, Chef Software, Inc.
+# Author:: Tim Smith <tsmith@chef.io>
+# Copyright:: 2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
