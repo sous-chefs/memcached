84c56aef1587430f0215936fd2c84487089fc71d
Tim Smith <tsmith@chef.io>
2017-02-23 22:37:42 -0800

    Require 12.5 and remove dependency on compat_resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,6 @@ version           '3.0.3'
 
 depends           'runit', '>= 1.2.0'
 depends           'yum-epel'
-depends           'compat_resource', '>= 12.16.3'
 
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
@@ -18,4 +17,4 @@ recipe 'memcached::default', 'Installs and configures memcached'
 
 source_url 'https://github.com/chef-cookbooks/memcached'
 issues_url 'https://github.com/chef-cookbooks/memcached/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.5' if respond_to?(:chef_version)
