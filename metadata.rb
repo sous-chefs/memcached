f60a5aa8d1a5a0b495b2a3c4f63319c1640c800f
Tim Smith <tsmith@chef.io>
2017-03-21 15:55:18 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'memcached'
 maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Installs memcached and includes memcached_instance resource for setting up runit memcached instances'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '4.0.0'
