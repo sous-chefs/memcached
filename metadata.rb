e9031a0dc7a3a44d08236cac3a5ab965be4c6660
Mark E. Schill <Mark.Schill@cmschill.net>
2019-10-20 21:29:40 -0400

    Closes #102

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,12 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs memcached and includes memcached_instance resource for setting up memcached instances'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version           '5.1.1'
 
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
 end
-
-recipe 'memcached::default', 'Installs and configures memcached'
 
 source_url 'https://github.com/chef-cookbooks/memcached'
 issues_url 'https://github.com/chef-cookbooks/memcached/issues'
